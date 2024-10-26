07440051dc171a75c66d97f819284156dd9cc5f8
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2024-10-26 08:56:13 +0530

    Update predict4.py

--- Tabs/predict4.py
+++ Tabs/predict4.py
@@ -100,5 +100,5 @@ def app(df, X, y):
         if result > 100: result = 100
         result = abs(result)
         # Display the prediction result
-        st.success(f"Cloud Security Optimization Level: {result:.2f} %")
-        st.sidebar.info("Prediction of Cloud Security based on various access controls")
+        st.success(f"Cloud Network Optimization Level: {result:.2f} %")
+        st.sidebar.info("Prediction of Cloud Network Mapping based on various access controls")
